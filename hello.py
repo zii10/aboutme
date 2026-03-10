b6017a87a8beda62d7ca37727c52d35988321648
zii10 <lekent1992@gmail.com>
2026-03-10 12:09:51 +0800

    week3.py

--- hello.py
+++ hello.py
@@ -1,14 +1,22 @@
 def square(y):
-	print(f"{y}的平方是{y**3}")
+    print("{} 的平方是 {}".format(y, y**2))
+
+def sum_to_n(n):
+    total = 0
+    for i in range(1, n+1):
+        total += i
+    return total
 
 
-x=int(input("請輸入一個整數:"))
-#x+=10
+x = int(input("請輸入一個整數:"))
 
-if(x<=0):
-	print(f"您輸入的值是{x}，小於等於0")
+if(x <= 0):
+    print("您輸入的值是{}，小於等於0".format(x))
 else:
-	print(f"您輸入的值是{x}，大於0")
-	for i in range(1,x+1):
-		#print(i，end=";")
-		square(i)
+    print("您輸入的值是{}，大於0".format(x))
+    
+    for i in range(1, x+1):
+        square(i)
+
+    s = sum_to_n(x)
+    print("1+2+...+{} 的總和是 {}".format(x, s))
